e747824f1ad0dcb1471a95343de910294958d59e
DreMukare <dremukare@gmail.com>
2023-06-16 01:32:01 +0300

    wip: created ref object in FirebaseWrapper

--- src/services/FirebaseService.ts
+++ src/services/FirebaseService.ts
@@ -3,6 +3,7 @@ import { when } from 'mobx';
 import * as FirebaseAuth from 'firebase/auth'
 import * as FirebaseDb from 'firebase/database'
 import * as FirebaseStorage from 'firebase/storage'
+import FirebaseWrapper from './FirebaseWrapper';
 
 class FirebaseService {
   public usersListPath = 'user_list'
@@ -10,8 +11,10 @@ class FirebaseService {
   public storage = FirebaseStorage.getStorage(Firebase)
   public database = FirebaseDb.getDatabase(Firebase)
 
-  ref = (ref: string, database?: string) => {
+  ref = (ref: string) => {
+    const wrapper = new FirebaseWrapper(ref, this.database)
     
+    return wrapper
   }
 
 }
--- src/services/FirebaseWrapper.ts
+++ src/services/FirebaseWrapper.ts
@@ -1,6 +1,13 @@
 import Firebase from '@/utilities/Firebase';
+import {Database, ref} from 'firebase/database'
 
 class FirebaseWrapper {
+  public wrapperRef: any;
+  
+  constructor(refString: string, database: Database) {
+    this.wrapperRef = ref(database, refString);
+  }
+
 
 }
 
